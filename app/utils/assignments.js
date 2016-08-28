13ac3b86112903aebedd73cd24676d24845d2af1
Jacob LeGrone <git@jacoblegrone.com>
2016-08-27 23:13:10 -0400

    Use mock assignment data [temporary]

--- app/utils/assignments.js
+++ app/utils/assignments.js
@@ -1,6 +1,13 @@
-const BASE_URL = 'https://organize.berniesanders.com';
+// const BASE_URL = 'https://organize.berniesanders.com';
 
+// export function getAssignments () {
+//   return fetch(`${BASE_URL}/contact-assignments.json?platform=android`)
+//   .then(response => response.json());
+// }
+
+import mock from '../../mock/assignments.json';
 export function getAssignments () {
-  return fetch(`${BASE_URL}/contact-assignments.json?platform=android`)
-  .then(response => response.json());
+  return new Promise(resolve => {
+    setTimeout(() => resolve(mock), 200);
+  });
 }
