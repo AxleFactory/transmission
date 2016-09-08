4e02794b22572ed6ce644115daf7f857ce591774
Jacob LeGrone <git@jacoblegrone.com>
2016-09-07 22:34:14 -0400

    Fill in current contact's first name in action titles

--- app/components/assignment/rows/call.js
+++ app/components/assignment/rows/call.js
@@ -3,9 +3,10 @@ import AssignmentRow from './row';
 import I18n from '../../../localization';
 
 export default function CallRow ({contact, callAction, ...props}) {
+  const contactName = contact ? contact.firstName : I18n.t('assignments.nameFallback');
   return (
     <AssignmentRow
-      title={I18n.t('assignments.options.call.title')}
+      title={I18n.t('assignments.options.call.title', {contactName})}
       text={contact ? callAction.callScript : callAction.name}
       icon="phone"
       enabled={!!contact}
--- app/components/assignment/rows/text.js
+++ app/components/assignment/rows/text.js
@@ -3,9 +3,10 @@ import AssignmentRow from './row';
 import I18n from '../../../localization';
 
 export default function TextRow ({contact, ...props}) {
+  const contactName = contact ? contact.firstName : I18n.t('assignments.nameFallback');
   return (
     <AssignmentRow
-      title={I18n.t('assignments.options.text.title')}
+      title={I18n.t('assignments.options.text.title', {contactName})}
       icon="comment"
       {...props}
     />
